0d9f043a13e1e896ed4ff3f614fb79f48aa59670
e.lascano <emilas5678@gmail.com>
2023-03-04 19:19:50 -0500

    add signup service

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,28 @@
 import * as tokenService from './tokenService'
 const BASE_URL = '/api/auth'
 
+
+
+async function signup(user) {
+  try {
+    const res = await fetch(`${BASE_URL}/signup`, {
+      method: 'POST',
+      headers: new Headers({ 'Content-Type': 'application/json' }),
+      body: JSON.stringify(user),
+    })
+    const json = await res.json()
+    if (json.token) {
+      tokenService.setToken(json.token)
+    }
+    if (json.err) {
+      throw new Error(json.err)
+    }
+  } catch (err) {
+    throw err
+  }
+}
+
+
 function getUser() {
   return tokenService.getUserFromToken()
 }
@@ -11,5 +33,6 @@ function getUser() {
 
 
 export {
+  signup,
   getUser,
 }
