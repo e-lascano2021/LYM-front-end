46c1b3af26b94ec38987ca6b2118f077e1891870
e.lascano <emilas5678@gmail.com>
2023-03-05 16:22:15 -0500

    create profileService

---
+++ src/services/profileService.js
@@ -0,0 +1,23 @@
+import * as tokenService from './tokenService'
+const BASE_URL = `${process.env.REACT_APP_BACK_END_SERVER_URL}/api/profiles`
+
+
+
+async function addPhoto(photoData, id) {
+  try {
+    const res = await fetch(`${BASE_URL}/${id}/add-photo`, {
+      method: 'PUT',
+      headers: {
+        'Authorization': `Bearer ${tokenService.getToken()}`
+      },
+      body: photoData
+    })
+    return await res.json()
+  } catch (error) {
+    throw error
+  }
+}
+
+export {
+  addPhoto
+}
