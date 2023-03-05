45cb485c924bb12ce0574680d36558bbe8a77784
e.lascano <emilas5678@gmail.com>
2023-03-04 19:21:22 -0500

    add login service

--- src/services/authService.js
+++ src/services/authService.js
@@ -27,11 +27,30 @@ function getUser() {
   return tokenService.getUserFromToken()
 }
 
+
 function logout() {
   tokenService.removeToken()
 }
 
 
+async function login(credentials) {
+  try {
+    const res = await fetch(`${BASE_URL}/login`, {
+      method: 'POST',
+      headers: new Headers({ 'Content-Type': 'application/json' }),
+      body: JSON.stringify(credentials),
+    })
+    const json = await res.json()
+    if (json.token) {
+      tokenService.setToken(json.token)
+    }
+    if (json.err) {
+      throw new Error(json.err)
+    }
+  } catch (err) {
+    throw err
+  }
+}
 
 
 
@@ -39,5 +58,6 @@ function logout() {
 export {
   signup,
   getUser,
-  logout
+  logout,
+  login
 }
