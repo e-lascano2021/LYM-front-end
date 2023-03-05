6f3b917bb788109908078b5827323c936ef3cb59
e.lascano <emilas5678@gmail.com>
2023-03-05 13:03:25 -0500

    create NavBar Component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 // import logo from './logo.svg';
 import { useState} from 'react'
-import { Routes, Route} from 'react-router-dom'
+import { Routes, Route, useNavigate} from 'react-router-dom'
 import './App.css';
 
 
 import * as authService from './services/authService'
 import Landing from './pages/Landing/Landing.jsx';
+import NavBar from './components/NavBar/NavBar';
 
 
 function App() {
   const [user, setUser] = useState(authService.getUser())
-  
+  const navigate = useNavigate()
 
 
   const handleSignupOrLogin = () => {
     setUser(authService.getUser())
   }
 
-
+  const handleLogout = () => {
+    authService.logout()
+    setUser(null)
+    navigate('/')
+  }
 
   return (
     <>
+      <NavBar handleLogout={handleLogout}/>
       <Routes>
         <Route path="/" element={<Landing handleSignupOrLogin={handleSignupOrLogin}/>} />
       </Routes>
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom'
+
+const NavBar = ({ handleLogout }) => {
+  return (
+    <nav>
+      <ul>
+        <li><Link to="/loveArmy">LoveArmy</Link></li>
+        <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
+        <li><Link to="/changePassword">Change Password</Link></li>
+        <li><Link to="/profile">My Profile</Link></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default NavBar
