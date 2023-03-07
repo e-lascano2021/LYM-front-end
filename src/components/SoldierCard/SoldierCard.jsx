86d53690d3863685da5bb0cfe2e80ca12c9a1a40
e.lascano <emilas5678@gmail.com>
2023-03-06 20:33:29 -0500

    add basic styling to the loveArmy page

--- src/components/SoldierCard/SoldierCard.jsx
+++ src/components/SoldierCard/SoldierCard.jsx
@@ -1,13 +1,22 @@
+import styles from './SoldierCard.module.css'
 
-
-const SoldierCard = (props) => {
+const SoldierCard = ({soldier}) => {
 
 
   return(
-    <>
-    <div>{props.soldier.name}</div>
-    <div>{props.soldier.loveTypes}</div>
-    </>
+    <div className={styles.card}>
+      <img src='https://i.imgur.com/8qF3Faq_d.webp?maxwidth=520&shape=thumb&fidelity=high' alt='temporary'/>
+      <div className={styles.cardInfo}>
+        <h2>{soldier.name}</h2>
+        <br/>
+        <h6>Love Types:</h6>
+        <p>{soldier.loveTypes?.join(", ")}</p>
+        <br/>
+        <h6>Love Languages:</h6>
+        <p>{soldier.loveLanguages?.join(", ")}</p>
+      </div>
+
+    </div>
   )
 }
 
--- src/pages/Army/Army.jsx
+++ src/pages/Army/Army.jsx
@@ -1,10 +1,10 @@
 import { Link } from 'react-router-dom'
 import SoldierCard from "../../components/SoldierCard/SoldierCard"
-
+import styles from './Army.module.css'
 const Army = (props) => {
   return (
     <main>
-      <div>
+      <div className={styles.header}>
         <h1>LoveArmy</h1>
         <Link to='/loveArmy/new'>
           <button>Add Soldier</button>
