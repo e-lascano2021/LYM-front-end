7d2249c2b3778f31b099738b811e505ed375dc19
e.lascano <emilas5678@gmail.com>
2023-03-14 18:06:11 -0400

    add styling to soldier page

--- src/pages/Soldier/Soldier.jsx
+++ src/pages/Soldier/Soldier.jsx
@@ -1,6 +1,8 @@
 import { useParams } from "react-router-dom"
 import { useEffect, useState} from 'react'
 import { getSoldier } from "../../services/armyService.js"
+import styles from "./Soldier.module.css"
+
 const Soldier = () => {
   let { id } = useParams()
   let [soldier, setSoldier] = useState(null)
@@ -17,22 +19,24 @@ const Soldier = () => {
   if(soldier.err) return <h1>{soldier.err}</h1>
 
   return (
-    <main>
+    <main className={styles.main}>
 
-      <div>
-        {/* <img src={soldier.image} alt={`${soldier.name}`}></img> */}
-        <h2>{soldier.name}</h2>
-      </div>
+      <div className={styles.header}>
+        <div className={styles.imageContainer}>
+          <img src={soldier.image} alt={`${soldier.name}`}></img>
+          <h2>{soldier.name}</h2>
+        </div>
 
-      <div>
-        <h6>Love Types: </h6>
-        <p>{soldier.loveTypes?.join(", ")}</p>
-        <br/>
-        <h6>Love Languages: </h6>
-        <p>{soldier.loveLanguages?.join(", ")}</p>
-        <br/>
-        <h6>Brownie Points:</h6>
-        <p>{soldier.currentPoints}/{soldier.totalPoints}</p>
+        <div className={styles.headerInfo}>
+          <h6>Love Types: </h6>
+          <p>{soldier.loveTypes?.join(", ")}</p>
+          <br/>
+          <h6>Love Languages: </h6>
+          <p>{soldier.loveLanguages?.join(", ")}</p>
+          <br/>
+          <h6>Brownie Points:</h6>
+          <p>{soldier.currentPoints}/{soldier.totalPoints}</p>
+        </div>
       </div>
 
     </main>
