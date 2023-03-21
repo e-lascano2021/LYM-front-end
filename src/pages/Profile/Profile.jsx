f3fd35f228d5b8dc87b16a7fcb905866dbe4bf7f
e.lascano <emilas5678@gmail.com>
2023-03-21 18:23:27 -0400

    display profile plans

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState} from 'react'
+import Plan from '../../components/Plan/Plan.jsx'
 import * as profileService from '../../services/profileService.js'
 
 const Profile = ({user}) => {
@@ -15,14 +16,19 @@ const Profile = ({user}) => {
   if (!profile) return <h1>Loading ...</h1>
 
   return (
-    <>
+    <main>
       <h1>{profile.name}</h1>
       <p>{profile.loveTypes.join(', ')}</p>
       <p>{profile.loveLanguages.join(', ')}</p>
       <p>{profile.plans[0].when}</p>
       <p>{profile.plans[0].where}</p>
       <p>{profile.plans[0].what}</p>
-    </>
+      <div>
+        {profile.plans.map((plan,idx) => 
+          <Plan key={idx} plan={plan}/>
+        )}
+      </div>
+    </main>
   )
 }
 
